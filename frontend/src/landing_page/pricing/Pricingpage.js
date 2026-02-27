4d11629d77b75fd6c8d9d2b02a5ef7e028d34fec
Singh.priyanshu1488@gmail.com <Singh.priyanshu1488@gmail.com>
2026-02-27 13:34:15 +0530

    Added Script

---
+++ frontend/src/landing_page/pricing/Pricingpage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Pricingpage() {
+    return ( 
+        <h1>Pricing page </h1>
+     );
+}
+
+export default Pricingpage;
---
+++ frontend/src/landing_page/support/Supportpage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Supportpage() {
+    return (  
+        <h1> Support page </h1>
+    );
+}
+
+export default Supportpage;
